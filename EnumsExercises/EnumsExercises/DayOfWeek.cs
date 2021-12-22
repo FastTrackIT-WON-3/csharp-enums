6636665a9244b5c67fbadbbee9aef6c4837d916e
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-12-22 21:04:23 +0200

    Exercises with enums

---
+++ EnumsExercises/EnumsExercises/DayOfWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnumsExercises
+{
+    [Flags]
+    public enum DayOfWeek
+    {
+        None = 0,
+        Monday    = 0b_0000_0001,
+        Tuesday   = 0b_0000_0010,
+        Wednesday = 0b_0000_0100,
+        Thursday  = 0b_0000_1000,
+        Friday    = 0b_0001_0000,
+        Saturday  = 0b_0010_0000,
+        Sunday    = 0b_0100_0000,
+
+        Weekend = Saturday | Sunday
+    }
+}
---
+++ EnumsExercises/EnumsExercises/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace EnumsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek day1 = DayOfWeek.Sunday;
+            // package.HasFlag(option)
+            if (DayOfWeek.Weekend.HasFlag(day1))
+            {
+                Console.WriteLine($"{day1} is part of {DayOfWeek.Weekend}");
+            }
+            else
+            {
+                Console.WriteLine($"{day1} is NOT part of {DayOfWeek.Weekend}");
+            }
+        }
+
+        static void FlagEnum_ManualTest()
+        {
+            DayOfWeek weekend = DayOfWeek.Saturday |
+                                DayOfWeek.Sunday;
+
+            DayOfWeek day1 = DayOfWeek.Monday;
+            if ((day1 & weekend) == day1)
+            {
+                Console.WriteLine($"{day1} is part of {weekend}");
+            }
+            else
+            {
+                Console.WriteLine($"{day1} is NOT part of {weekend}");
+            }
+        }
+
+        static void Enum_Parse()
+        {
+            string dayOfWeek = "None";
+            // bool isConvertible = Enum.TryParse(typeof(DayOfWeek), dayOfWeek, out object parseResult);
+            bool isConvertible = Enum.TryParse(dayOfWeek, out DayOfWeek parsedResult);
+            if (isConvertible)
+            {
+                DayOfWeek value = parsedResult;
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine(parsedResult);
+                Console.WriteLine($"'{dayOfWeek}' cannot be converted to DayOfWeek");
+            }
+        }
+
+        static void Enum_VerifyIfValueIsDefined()
+        {
+            int value = 8;
+
+            if (Enum.IsDefined(typeof(DayOfWeek), value))
+            {
+                DayOfWeek day = (DayOfWeek)value;
+                Console.WriteLine(day);
+            }
+            else
+            {
+                Console.WriteLine($"{value} doesn't represent a valid value");
+            }
+        }
+    }
+}
